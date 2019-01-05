8a7effeff12d17468b16c515189730b70452f746
Nandwana <Nandwana@Nandwana-PC>
2019-01-05 21:28:18 +0530

    Added Exception handler methods.

--- TodoApp/src/main/java/com/na/todo/controller/TodoController.java
+++ TodoApp/src/main/java/com/na/todo/controller/TodoController.java
@@ -2,15 +2,19 @@ package com.na.todo.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.na.todo.exception.NoSuchTodoException;
 import com.na.todo.exception.TodoAlredyExistsException;
 import com.na.todo.model.Todo;
 import com.na.todo.service.ITodoService;
@@ -50,4 +54,12 @@ public class TodoController {
 	private void deleteTodo(@PathVariable int srNo){
 		todoService.deleteTodo(srNo);
 	}
+	
+	@ExceptionHandler
+	@ResponseStatus(HttpStatus.PRECONDITION_FAILED)
+	private void todoAlreadyExistsExceptionHandler(TodoAlredyExistsException e){}
+	
+	@ExceptionHandler
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	private void todoAlreadyExistsExceptionHandler(NoSuchTodoException e){}
 }
