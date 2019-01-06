557405d6ffaf4764e3cb8298dce5831cb9967d9e
Nandwana <Nandwana@Nandwana-PC>
2019-01-06 19:32:08 +0530

    Added CommandLineRunner implementation.

--- TodoApp/src/main/java/com/na/todo/TodoMain.java
+++ TodoApp/src/main/java/com/na/todo/TodoMain.java
@@ -1,12 +1,19 @@
 package com.na.todo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodoMain {
+public class TodoMain implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoMain.class, args);
 	}
+
+  @Override
+  public void run(String... arg0) throws Exception {
+    System.out.println("This method gets called just after application context laoding, any initialzation can be done here.");
+  }
+  
 }
