5bf3ab28b45d745b4a86df0283c2b8c810fa5119
Nandwana <Nandwana@Nandwana-PC>
2019-01-12 16:44:25 +0530

    propsed model for this app.

--- TodoApp/src/main/resources/static/js/app/model/todos.js
+++ TodoApp/src/main/resources/static/js/app/model/todos.js
@@ -1,1 +1,24 @@
-var todos = []; 
+//want to create a model for this app, but unsure right now...
+var todoAppModel = {
+	'userId' : userId,
+	'role' : role,
+	'isCreateAllowed' : true,
+	'isWriteAllowed' : true,
+	'isDeleteAllowed' : true,
+	todos : [ {
+		'srNo' : 1,
+		'text' : todo1,
+		'completed' : false,
+		'completionDate' : null
+	}, {
+		'srNo' : 2,
+		'text' : todo2,
+		'completed' : false,
+		'completionDate' : null
+	}, {
+		'srNo' : 3,
+		'text' : todo3,
+		'completed' : false,
+		'completionDate' : null
+	} ]
+};
