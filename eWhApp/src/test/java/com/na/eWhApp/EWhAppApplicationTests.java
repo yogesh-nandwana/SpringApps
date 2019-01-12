3421b6b5779edc1b82a6a390ca14741dbcb611a2
Nandwana <Nandwana@Nandwana-PC>
2019-01-12 22:40:31 +0530

    Adding eWhApp

---
+++ eWhApp/src/test/java/com/na/eWhApp/EWhAppApplicationTests.java
@@ -0,0 +1,17 @@
+package com.na.eWhApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EWhAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
